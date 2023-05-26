8dbc6e3b553df197be5a7de13aeae336958a3f9c
Eric <esbev@hotmail.com>
2023-05-25 22:28:57 -0500

    added all hours of work day to html, started working on js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,37 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
+// the code isn"t run until the browser has finished rendering all the elements
 // in the html.
+var today = $("#currentDay");
+function updateTime() {
+  var currentTime = dayjs().format("MMM D, YYYY HH:mm:ss a");
+  today.text(currentTime);
+}
+// Update time initially
+// updateTime();
+// Update time every second
+setInterval(updateTime, 1000);
+
+function init() {
+  var hour9 = localStorage.getItem("hour-9");
+  $("hour-9").text(hour9);
+  var hour10 = localStorage.getItem("hour-10");
+  $("hour-10").text(hour10);
+  var hour11 = localStorage.getItem("hour-11");
+  $("hour-11").text(hour11);
+  var hour12 = localStorage.getItem("hour-12");
+  $("hour-12").text(hour12);
+  var hour13 = localStorage.getItem("hour-13");
+  $("hour-13").text(hour13);
+  var hour14 = localStorage.getItem("hour-14");
+  $("hour-14").text(hour14);
+  var hour15 = localStorage.getItem("hour-15");
+  $("hour-15").text(hour15);
+  var hour16 = localStorage.getItem("hour-16");
+  $("hour-16").text(hour16);
+  var hour17 = localStorage.getItem("hour-17");
+  $("hour-17").text(hour17);
+}
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +52,8 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+function saveEvent(event) {
+  var textArea = event.target.parentElement.querySelector("description");
+  localStorage.setItem(textArea.id, textArea.value);
+}
