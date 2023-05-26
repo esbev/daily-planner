b53aa5551fb65ef52424befcac908b412112b0c6
Eric <esbev@hotmail.com>
2023-05-26 18:59:16 -0500

    removed html code i wasn't using in the project, wrote most the js code today

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,59 +1,72 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn"t run until the browser has finished rendering all the elements
-// in the html.
-var today = $("#currentDay");
-function updateTime() {
-  var currentTime = dayjs().format("MMM D, YYYY HH:mm:ss a");
-  today.text(currentTime);
-}
-// Update time initially
-// updateTime();
-// Update time every second
-setInterval(updateTime, 1000);
+var hourBlockAreaText;
+var currentHour = dayjs().format("H");
+var today = $("#current-day");
+var currentDay = dayjs().format("dddd, MMM D, YYYY");
+today.text(currentDay);
+$("#current-hour").text(currentHour);
 
 function init() {
-  var hour9 = localStorage.getItem("hour-9");
-  $("hour-9").text(hour9);
-  var hour10 = localStorage.getItem("hour-10");
-  $("hour-10").text(hour10);
-  var hour11 = localStorage.getItem("hour-11");
-  $("hour-11").text(hour11);
-  var hour12 = localStorage.getItem("hour-12");
-  $("hour-12").text(hour12);
-  var hour13 = localStorage.getItem("hour-13");
-  $("hour-13").text(hour13);
-  var hour14 = localStorage.getItem("hour-14");
-  $("hour-14").text(hour14);
-  var hour15 = localStorage.getItem("hour-15");
-  $("hour-15").text(hour15);
-  var hour16 = localStorage.getItem("hour-16");
-  $("hour-16").text(hour16);
-  var hour17 = localStorage.getItem("hour-17");
-  $("hour-17").text(hour17);
+  buildHourBlocks();
+  populateTextAreas();
 }
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+function buildHourBlocks() {
+  var hourBlockID = 9;
+  var timeBlocksText = [9,10,11,12,1,2,3,4,5];
+  for (i in timeBlocksText) {
+    var timeBlockTime = timeBlocksText[i];
+    if (i <= 2) {
+      timeBlockTime += "AM";
+    } else {
+      timeBlockTime += "PM"
+    }
 
-function saveEvent(event) {
-  var textArea = event.target.parentElement.querySelector("description");
-  localStorage.setItem(textArea.id, textArea.value);
+    var hourBlockEl = "";
+    hourBlockEl += '<div id="hour-' + hourBlockID + '" class="row time-block">';
+    hourBlockEl += '<div class="col-2 col-md-1 hour text-center py-3">' + timeBlockTime + "</div>";
+    hourBlockEl += '<textarea class="col-8 col-md-10 description" rows="3"></textarea>';
+    hourBlockEl += '<button class="btn saveBtn col-2 col-md-1" aria-label="save">';
+    hourBlockEl += '<i class="fas fa-save" aria-hidden="true"></i>';
+    hourBlockEl += "</button>";
+    hourBlockEl += "</div>";
+    $("#hour-block-container").append(hourBlockEl);
+    hourBlockID++;
+  }
 }
+
+function populateTextAreas() {
+  hourBlockAreaText = {...localStorage};
+  for (i in hourBlockAreaText) {
+    $(".time-block").each(function () {
+      var content = hourBlockAreaText[$(this).attr("id")];
+      $(this).children("textarea").val(content);
+    });
+  }
+}
+
+function applyHourBlockColor() {
+  $(".time-block").each(function () {
+    var hourID = $(this).attr("id").split("-")[1];
+    if (hourID < currentHour) {
+      $(this).addClass("past");
+    } else if (hourID == currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  })
+}
+
+$(document).ready(function() {
+  init();
+  setInterval(applyHourBlockColor, 1000);
+
+  $(".time-block .saveBtn").on("click", function () {
+    var textAreaID = $(this).parents(".time-block").attr("id");
+    var textAreaText = $(this).siblings("textarea").val();
+    localStorage.setItem(textAreaID, textAreaText);
+  });
+})
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# daily-planner
+# Module 5 Challenge - Daily Planner 
+
+## Description
+
+type stuff here
+
+## Table of Contents
+- [Website Preview](#website-preview)
+- [Credits](#credits)
+    - [JavaScript Resources](#javascript-resources)
+        - [Instructor Aided Code](#instructor-aided-code)
+        - [Quiz Questions](#quiz-questions)
+        - [W3schools.com](#w3schoolscom)
+        - [Mozilla.org](#mozillaorg)
+- [Special Thanks](#special-thanks)
+- [License](#license)
+
+## Website Preview
+
+[Password Generator](https://esbev.github.io/Coding-Quiz/)
+
+[![name](assets/images/SS.jpg)](https://esbev.github.io/Coding-Quiz/)
+
+## Credits
+
+NOTE: Click on a thumbnail to view source
+
+- ### JavaScript Resources
+
+    - #### Instructor Aided Code
+
+        Instructor Eli Montoya provided a snippet of example code I followed very closely for a portion of this project
+
+     - ![name](assets/images/highscores.jpg)
+
+    - #### Quiz Questions
+
+        Bootcamp student Joshua Stanford aided in providing the quiz questions I used for the project
+
+    - #### W3schools.com
+
+     - [![name](assets/images/w3schools.jpg)](https://www.w3schools.com/js/default.asp)
+
+    - #### Mozilla.org
+
+     - [![name](assets/images/mozilla.jpg)](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference)
+
+
+## Special Thanks
+
+Thanks goes out to all the instructors and teacher assistants for their clear instruction and prompt and thorough answers to all my questions. Also, to all the students who joined in the study groups for their active participation and alternative perspectives. I was able to gain multiple insights I otherwise may not have gained towards the project without them. There is always strength in numbers.
+
+## License
+
+MIT License
+
+Copyright (c) 2023 Eric
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
